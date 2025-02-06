5654a56f195b7f99bd9bfdcb608131925a050867
Icaro Siqueira <icarorob@hotmail.com>
2025-02-06 10:44:14 -0300

    Fix when address_info is not present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @address_info = Cep::Client.address_info_by_cep(params[:cep][:number]) if params[:cep][:number].present?
+    @address_info = Cep::Client.address_info_by_cep(params[:cep][:number]) if params.dig(:cep, :number).present?
   rescue Cep::Exception => e
     @error_message = e.message
   end
