e20ef92714b8d7c7cafb5398e4d06ab3ff763ece
Icaro Siqueira <icarorob@hotmail.com>
2025-02-06 16:32:41 -0300

    Add data to cards on views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
+  before_action :set_most_searched_ceps
+
   def index
     @address_info = Cep::Client.address_info_by_cep(params[:cep][:number]) if params.dig(:cep, :number).present?
   rescue Cep::Exception => e
     @error_message = e.message
+  end
+
+  def set_most_searched_ceps
+    @most_searched_ceps = CepSearch.most_searched_ceps
+    @most_searched_ceps_from_each_state = CepSearch.most_searched_ceps_from_each_state
   end
 end
--- app/models/cep_search.rb
+++ app/models/cep_search.rb
@@ -1,3 +1,8 @@
 class CepSearch < ApplicationRecord
   validates_uniqueness_of :number
+
+  scope :most_searched_ceps, -> { order(count: :asc).last(5).reverse }
+  scope :most_searched_ceps_from_each_state, -> {
+    pluck(:uf).uniq.map { |uf| CepSearch.where(uf: uf).order(count: :asc).last }
+  }
 end
